53fb4b6d6615948ae63ed34f69d18ab799df04d5
prasadmate28 <prasad_mate@hotmail.com>
2018-02-20 22:55:31 -0500

    VIF method update

--- src/main/scala/LinearRegressionAnalysis.scala
+++ src/main/scala/LinearRegressionAnalysis.scala
@@ -3,7 +3,7 @@ import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.udf
-
+import scala.collection.mutable.ListBuffer
 
 object LinearRegressionAnalysis extends App {
 
@@ -16,14 +16,12 @@ object LinearRegressionAnalysis extends App {
       features and label using Vector Assembler
   */
 
-  def loadTrainingData(data:DataFrame, featureCols: Array[String], classLabel: String):DataFrame={
+  def loadTrainingData(data:DataFrame, featureCols: Array[String], responseLabel: String):DataFrame={
     //val featureCols = Array("X1","X2","X3","X4","X5","X6","X7","X8","X9","X10","X11","X12","X13","X14","X15")
     //val featureCols = data.schema.fieldNames.drop(data.schema.fieldNames.length)
     //dropRight(1)
-    //val classLabel = data.schema.fieldNames.last
-    //val classLabel = "Y"
     println("Features :" + featureCols.toList)
-    println("Label col: "+classLabel)
+    println("Label col: "+responseLabel)
     val assembler =  new VectorAssembler()
       .setInputCols(featureCols)
       .setOutputCol("features")
@@ -40,7 +38,7 @@ object LinearRegressionAnalysis extends App {
     This method builds a training model from the dataframe given.
   */
   def buildModel(trainingData: DataFrame): LinearRegressionModel ={
-    val lr = new LinearRegression().setFeaturesCol("features").setLabelCol("Y")
+    val lr = new LinearRegression().setFeaturesCol("features").setLabelCol(responseLabel)
       //.setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
 
     val model = lr.fit(trainingData)
@@ -78,9 +76,9 @@ object LinearRegressionAnalysis extends App {
   /*
     This method elvaluates Variance Inflation Factor to test multicollinearity between the attributes.
   */
+
   def calcVIF(trainingData: DataFrame) = {
-    var x = 0
-    var vif_coeff = Seq [Double]()
+    var vif_coeff = new ListBuffer [Double]()
 
     for(x <- featureCols){
 
@@ -90,8 +88,8 @@ object LinearRegressionAnalysis extends App {
       var df = loadTrainingData(trainingData,features.toArray,setLabel)
       var rsq = buildModel(df).summary.r2
       var vif = 1/(1-rsq)
-      println("!@#$^&*(*^%$@!@#$%^&* VIF \t :"+ vif)
-      vif_coeff:+ vif
+      println("!@#$^&*(*^%$@!@#$%^&* VIF \t :"+ vif + "for " + x)
+      vif_coeff += vif
 
     }
     println(s"VIF: \t" + vif_coeff)
@@ -99,25 +97,22 @@ object LinearRegressionAnalysis extends App {
 
  //---------------------------------------------------------------------------------------------
 
-  var fileName:String = "dataset-p1.csv"
-  //var fileName:String = args(0)
+  //var fileName:String = "dataset-p1.csv"
+  var fileName:String = args(0)
 
   val dataFile = spark.read.format("csv")
     .option("header","true")
     .option("inferSchema","true")
-    .load(fileName) //replace with args(0)
+    .load(fileName)
 
-  val classLabel = dataFile.schema.fieldNames.last
-
-  //var labelIndex = dataFile.schema.fieldIndex(classLabel)
+  val responseLabel = dataFile.schema.fieldNames.last
   var featureCols = Array("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15")
-    //dataFile.schema.fieldNames.drop(labelIndex)
 
   //  Run linear regression model with given dataset
-  val trainingData = loadTrainingData(dataFile, featureCols, classLabel)
+  val trainingData = loadTrainingData(dataFile, featureCols, responseLabel)
   println("======== Linear Regression --================")
-  //val model = buildModel(trainingData)
-
+  val model1 = buildModel(trainingData)
+  displaySummaryStatistics(model1)
   println(" -~~~~~~~~   Calculating VIF  ~~~~~~~~~~")
   calcVIF(dataFile)
 
@@ -127,21 +122,19 @@ object LinearRegressionAnalysis extends App {
   var transformed_dataFile = dataFile.withColumn("X4sq",addColumnUDF(dataFile.col("X4")))
 
   featureCols = Array("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15", "X4sq")
-    //dataFile.schema.fieldNames.drop(labelIndex)
   transformed_dataFile.show(5)
-  val trainingDataAugmented = loadTrainingData(transformed_dataFile,featureCols,classLabel)
+  val trainingDataAugmented = loadTrainingData(transformed_dataFile,featureCols,responseLabel)
   println("======== Polynomial Regression --================")
-  //buildModel(trainingDataAugmented)
+  val model2 = buildModel(trainingDataAugmented)
+  displaySummaryStatistics(model2)
 
   // removing redundunt features
   transformed_dataFile = dataFile.select("X1", "X2", "X4", "X5", "X7", "X8", "X9", "X10", "X12", "X14", "X15", "Y")
   transformed_dataFile.show(5)
-  //labelIndex = dataFile.schema.fieldIndex(classLabel)
   featureCols = Array("X1", "X2", "X4", "X5", "X7", "X8", "X9", "X10", "X12", "X14", "X15")
-    //dataFile.schema.fieldNames.drop(labelIndex)
-  val trainingData2 = loadTrainingData(transformed_dataFile,featureCols,classLabel)
+  val trainingData2 = loadTrainingData(transformed_dataFile,featureCols,responseLabel)
 
   println("======== Reduced feature Regression ================")
-  //buildModel(trainingData2)
-
+  val model3 = buildModel(trainingData2)
+  displaySummaryStatistics(model3)
 }
