1cdbbbef67bf8ebe927866b43f545ef0d1e7c5d6
prasadmate28 <prasad_mate@hotmail.com>
2018-02-20 21:45:13 -0500

    Updates of VIF

--- src/main/scala/LinearRegressionAnalysis.scala
+++ src/main/scala/LinearRegressionAnalysis.scala
@@ -11,6 +11,11 @@ object LinearRegressionAnalysis extends App {
     .appName("Linear Regression Analysis")
     .getOrCreate()
 
+  /*
+      This method takes dataframe obtained by reading csv file and converts it into dataframe containing
+      features and label using Vector Assembler
+  */
+
   def loadTrainingData(data:DataFrame, featureCols: Array[String], classLabel: String):DataFrame={
     //val featureCols = Array("X1","X2","X3","X4","X5","X6","X7","X8","X9","X10","X11","X12","X13","X14","X15")
     //val featureCols = data.schema.fieldNames.drop(data.schema.fieldNames.length)
@@ -18,30 +23,36 @@ object LinearRegressionAnalysis extends App {
     //val classLabel = data.schema.fieldNames.last
     //val classLabel = "Y"
     println("Features :" + featureCols.toList)
-    println("last col "+classLabel)
+    println("Label col: "+classLabel)
     val assembler =  new VectorAssembler()
       .setInputCols(featureCols)
       .setOutputCol("features")
 
     val df2 = assembler.transform(data)
-
-    println("First 5 rows")
-    df2.show(5)
+    println(s"Size of dataset" + df2.count())
+    //println("First 5 rows")
+    //df2.show(5)
     //df2.select("features").show(20,false)
     return df2
   }
 
+  /*
+    This method builds a training model from the dataframe given.
+  */
   def buildModel(trainingData: DataFrame): LinearRegressionModel ={
     val lr = new LinearRegression().setFeaturesCol("features").setLabelCol("Y")
       //.setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
 
     val model = lr.fit(trainingData)
 
-    displaySummaryStatistics(model)
+    //displaySummaryStatistics(model)
 
     return model
   }
 
+  /*
+      This method displays required summary of the model passed as a parameter
+  */
   def displaySummaryStatistics (model:LinearRegressionModel):Unit = {
     // Print the coefficients and intercept for linear regression
     println(s"Coefficients: ${model.coefficients} Intercept: ${model.intercept}")
@@ -64,9 +75,29 @@ object LinearRegressionAnalysis extends App {
 
   }
 
-  def calcVIF(trainingData: DataFrame, model: LinearRegressionModel) = {
+  /*
+    This method elvaluates Variance Inflation Factor to test multicollinearity between the attributes.
+  */
+  def calcVIF(trainingData: DataFrame) = {
+    var x = 0
+    var vif_coeff = Seq [Double]()
 
+    for(x <- featureCols){
+
+      var features = featureCols.toBuffer
+      features -= x
+      var setLabel = x
+      var df = loadTrainingData(trainingData,features.toArray,setLabel)
+      var rsq = buildModel(df).summary.r2
+      var vif = 1/(1-rsq)
+      println("!@#$^&*(*^%$@!@#$%^&* VIF \t :"+ vif)
+      vif_coeff:+ vif
+
+    }
+    println(s"VIF: \t" + vif_coeff)
   }
+
+ //---------------------------------------------------------------------------------------------
 
   var fileName:String = "dataset-p1.csv"
   //var fileName:String = args(0)
@@ -85,10 +116,10 @@ object LinearRegressionAnalysis extends App {
   //  Run linear regression model with given dataset
   val trainingData = loadTrainingData(dataFile, featureCols, classLabel)
   println("======== Linear Regression --================")
-  val model = buildModel(trainingData)
+  //val model = buildModel(trainingData)
 
   println(" -~~~~~~~~   Calculating VIF  ~~~~~~~~~~")
-  //calcVIF(trainingData, model)
+  calcVIF(dataFile)
 
   // Augment X4^2 column to the data and run linear regression
   val squaredColumn : (Double) => Double = (x4:Double) => math.pow(x4,2)
@@ -100,7 +131,7 @@ object LinearRegressionAnalysis extends App {
   transformed_dataFile.show(5)
   val trainingDataAugmented = loadTrainingData(transformed_dataFile,featureCols,classLabel)
   println("======== Polynomial Regression --================")
-  buildModel(trainingDataAugmented)
+  //buildModel(trainingDataAugmented)
 
   // removing redundunt features
   transformed_dataFile = dataFile.select("X1", "X2", "X4", "X5", "X7", "X8", "X9", "X10", "X12", "X14", "X15", "Y")
@@ -111,6 +142,6 @@ object LinearRegressionAnalysis extends App {
   val trainingData2 = loadTrainingData(transformed_dataFile,featureCols,classLabel)
 
   println("======== Reduced feature Regression ================")
-  buildModel(trainingData2)
+  //buildModel(trainingData2)
 
 }
